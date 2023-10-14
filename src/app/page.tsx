8e19b8c8e7b52e3c0754c93b47a8f40a1ebb3df0
Ryosuke Sato <postscaperyosuke@gmail.com>
2023-10-14 16:27:55 +0900

    change text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,25 @@
-'use client';
+"use client";
 
-import { Amplify } from 'aws-amplify';
-import awsconfig from "../aws-exports"
+import { Amplify } from "aws-amplify";
+import awsconfig from "../aws-exports";
 Amplify.configure(awsconfig);
 
-import '@aws-amplify/ui-react/styles.css';
-import { Authenticator } from '@aws-amplify/ui-react';
+import "@aws-amplify/ui-react/styles.css";
+import { Authenticator } from "@aws-amplify/ui-react";
 
-import styles from './page.module.css'
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Authenticator>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user?.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}
-    </Authenticator>
+        {({ signOut }) => (
+          <main>
+            <h1>ログイン成功です</h1>
+            <button onClick={signOut}>Sign out</button>
+          </main>
+        )}
+      </Authenticator>
     </main>
-  )
+  );
 }
